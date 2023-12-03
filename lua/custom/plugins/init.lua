9f451bc1b3007223566608a30ce29fb85f9df9b6
rs <rui@fastmail.com>
2023-12-03 06:19:43 +0000

    Php Config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,42 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	-- Lsp for php projects
+	{
+		'phpactor/phpactor',
+		opt = true,
+		ft = {'php'},
+		run = 'composer install --no-dev -o'
+	},
+
+	-- Laravel plugin
+	{
+		"adalessa/laravel.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"tpope/vim-dotenv",
+			"MunifTanjim/nui.nvim",
+		},
+		cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+		keys = {
+			{ "<leader>la", ":Laravel artisan<cr>" },
+			{ "<leader>lr", ":Laravel routes<cr>" },
+			{ "<leader>lm", ":Laravel related<cr>" },
+			{
+				"<leader>lt",
+				function()
+					require("laravel.tinker").send_to_tinker()
+				end,
+				mode = "v",
+				desc = "Laravel Application Routes",
+			},
+		},
+		event = { "VeryLazy" },
+		config = function()
+			require("laravel").setup()
+			require("telescope").load_extension "laravel"
+		end,
+	}
+}
+
